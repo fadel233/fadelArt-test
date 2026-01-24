f7500d382b34bc35136d57a17b6d07f9668bd239
fadel <fadeldalmeida3@gmail.com>
2026-01-24 20:37:16 +0100

    correction

--- app/src/Kernel.php
+++ app/src/Kernel.php
@@ -8,4 +8,18 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function registerBundles(): iterable
+    {
+        $contents = require $this->getProjectDir().'/config/bundles.php';
+        foreach ($contents as $class => $envs) {
+            // Skip si le bundle n'existe pas en prod
+            if ($this->environment === 'prod' && !class_exists($class)) {
+                continue;
+            }
+            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
+                yield new $class();
+            }
+        }
+    }
 }
